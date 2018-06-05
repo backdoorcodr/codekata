99cc59663d103e177a750e9600de41a1d6c41c4b
MuhammadSarimZafar <backdoorcoder@outlook.com>
2018-06-05 16:40:55 +0200

    fix(RomanNumerals): handle all roman numerals

--- src/src/RomanNumeralsTest.java
+++ src/src/RomanNumeralsTest.java
@@ -4,7 +4,11 @@ import org.junit.*;
 import org.junit.runner.RunWith;
 import static org.junit.Assert.*;
 
+import java.awt.List;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 
 import static org.hamcrest.CoreMatchers.*;
 import org.junit.runners.Parameterized;
@@ -17,7 +21,7 @@ public class RomanNumeralsTest {
 	public static Iterable<Object[]> data() 
 	{
 		return Arrays.asList(new Object[][] { 
-			{ 0, "" }, { 1, "I" }, { 2, "II" } , { 5, "V" }
+			{ 0, "" }, { 1, "I" }, { 2, "II" } , { 5, "V" }, { 6, "VI" }, { 10, "X" }, { 20, "XX" }, { 4, "IV" }, {1984, "MCMLXXXIV"}
 	});
 	}
 
@@ -36,10 +40,31 @@ public class RomanNumeralsTest {
 
 	private String toRomanNumber(int i) {
 		
+		HashMap<Integer, String> map = new LinkedHashMap<Integer, String>();
+		map.put(1000, "M");
+		map.put(900, "CM");
+		map.put(500, "D");
+		map.put(400, "CD");
+		map.put(100, "C");
+		map.put(90, "XC");
+		map.put(50, "L");
+		map.put(40, "XL");
+		map.put(10, "X");
+		map.put(5, "V");
+		map.put(4, "IV");
+		map.put(1, "I");
+		
 		String result = "";
-		for (int idx = 0; idx < i; idx++) {
-			result = result + "I";
+		int difference = i;
+
+		for (int key : map.keySet()) 
+		{
+		while (difference >= key) {
+			result += map.get(key);
+			difference -= key;
 		}
+		}
+		
 		return result;
 	}
 
