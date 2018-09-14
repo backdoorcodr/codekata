788370c20421c0937ca2e29079a336731c7187e4
MuhammadSarimZafar <backdoorcoder@outlook.com>
2018-09-14 15:32:31 +0200

    use streams for JadenCase

--- src/src/JadenCase.java
+++ src/src/JadenCase.java
@@ -1,34 +1,23 @@
 package src;
 
-public class JadenCase {
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
-	static String regex = "\\W+";
-	
+public class JadenCase {
 
 	public String toJadenCase(String phrase) {
 		if ("".equals(phrase) || phrase == null) {
 			return null;
 		}
-		String result = "";
-		String[] words = phrase.split(regex);
-		
-		for (String word: words) {
-			result = result + getUpperCaseWord(word) + " ";
-		}
-		return result.trim();
-	}
-	
-	String getUpperCaseWord(String word) {
-		return word.substring(0, 1).toUpperCase() + word.substring(1);
+		return Arrays.asList(phrase.split(" ")).stream().map(word -> {
+            return Character.toUpperCase(word.charAt(0)) + word.substring(1);
+        }).collect(Collectors.joining(" "));
 	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		JadenCase o = new JadenCase();
-		System.out.println(o.toJadenCase("to upper case"));
-		
-		
-		
+		System.out.println(o.toJadenCase("aaa upper case"));
 
 	}
 
